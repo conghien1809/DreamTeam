462049dca1f2eef798a9d0df2d412cbd0dd7a23e
Nguyen Cong Hien <nguyenhien.1809202@gmail.com>
2024-12-10 19:33:55 +0700

    Thêm các file mới

---
+++ page/dichvu/index.php
@@ -0,0 +1,24 @@
+    <!-- Party Booking -->
+    <div class="party-booking">
+        <div class="inner-wrap">
+            <form action="">
+                <strong class="inner-title">NHẬN TƯ VẤN & BÁO GIÁ</strong>
+                <div class="inner-form-wrap">
+                    <input type="text" placeholder="Họ Và Tên" />
+                    <input type="text" placeholder="Số Điện Thoại" />
+                    <input type="email" placeholder="Email" />
+                    <input type="number" placeholder="Số Lượng Khách" />
+                    <input type="datetime-local" />
+                    <textarea placeholder="Thêm ghi chú"></textarea>
+                </div>
+                <div class="inner-button">
+
+                    <button class="button"><a href="./new-dishes.html">Liên Hệ Đặt Ngay</a></button>
+                </div>
+            </form>
+            <div class="inner-picture">
+                <img src="./assets/images/picture.jpg" alt="Hình ảnh" />
+            </div>
+        </div>
+    </div>
+    <!--End Party Booking -->
